3f58d3067f2dcf3f26c5e757df580232f00fa359
Mohamed <mfarah37@gmail.com>
2022-12-14 16:44:26 -0800

    footer & header throughout app

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,10 +2,9 @@ import './App.css';
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import WorkoutsPage from '../WorkoutsPage/WorkoutsPage';
-import NavBar from '../../components/NavBar/NavBar'
+import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
 import{ getUser } from '../../utilities/users-service'
 
 export default function App() {
@@ -17,10 +16,9 @@ export default function App() {
         <>
           <NavBar user={user} setUser={setUser}/>
           <Routes>
-            <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
             <Route path="/workouts" element={<WorkoutsPage />} />
           </Routes>
+          <Footer />
         </>
         :
         <AuthPage setUser={setUser} /> 
