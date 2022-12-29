2a8dadeec41a6378d79becdfc3a4343490928430
aaayon91 <aaayon91@gmail.com>
2022-12-29 15:51:56 -0800

    adds react icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import "./Footer.css"
+import {CgProfile} from 'react-icons/cg'
+import {BiQrScan} from 'react-icons/bi'
+import {IoBarbell} from 'react-icons/io5'
 
 export default function Footer() {
     return (
         <footer>
-            <button className="footerButton">PROFILE</button>
-            <button className="footerButton">SCAN</button>
-            <button className="footerButton">EXERCISE</button>
+            <CgProfile className="footerButton"/>
+            <BiQrScan className="footerButton"/>
+            <IoBarbell className="footerButton"/>
         </footer>
     )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import * as userService from '../../utilities/users-service'
 import "./NavBar.css"
+import {IoSettingsOutline} from 'react-icons/io5'
+import {CgSearch} from 'react-icons/cg'
 
 export default function NavBar({ user, setUser }) {
     
@@ -11,8 +13,10 @@ export default function NavBar({ user, setUser }) {
     }
     return(
         <nav>
-            <button className="navButton" id="settingsBtn">SETTINGS</button>
-            <button className="navButton" id="searchBtn">SEARCH</button>
+            {/* <button className="navButton" id="settingsBtn">SETTINGS</button> */}
+            <IoSettingsOutline className="navButton" id="settingsBtn"/>
+            <CgSearch className="navButton"/>
+            {/* <button className="navButton" id="searchBtn">SEARCH</button> */}
             {/* <Link to="/orders">Order History</Link>
             &nbsp;&nbsp;<Link to="/orders/new">New Order</Link>
             &nbsp;&nbsp;<span>Welcome, {user.name}</span>
